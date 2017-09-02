281c1d183fc8efa0874ef53958977f6ac96e57ce
dwyn <dwayne.harmon@gmail.com>
2017-09-02 15:14:49 -0400

    Seperate files for Project, Section and User controllers. Why: Because I saw the design pattern else where and thought it would be fun to use it. I hope I dont end up hating myself for it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,13 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    use Rack::Flash
   end
 
   get "/" do
+    @random_app_idea = []
+    @random_app_idea = Project.all
+    # binding.pry
     erb :welcome
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User <ActiveRecord::Base
   has_many :projects
   has_many :sections, through: :projects
 
-  validates_presence_of :username
-  validates_presence_of :email
-  validates_presence_of :password
+  # validates_presence_of :username
+  # validates_presence_of :email
+  # validates_presence_of :password
 
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
